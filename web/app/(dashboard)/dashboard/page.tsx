4249211956ba9f44080c9e66d6d9c7cd3c6636e7
yerayboix <yerayboix@gmail.com>
2025-10-17 13:32:20 +0200

    feat(sidebar): added sidebar from shadcn. Next step is configure it.

--- web/app/(dashboard)/dashboard/page.tsx
+++ web/app/(dashboard)/dashboard/page.tsx
@@ -1,19 +1,55 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
 
-export default async function DashboardPage() {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    });
-    if (!session) {
-        // This should never happen because of the middleware
-        return <div>Acceso denegado</div>;
-    }
-
-    return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">Dashboard</h1>
-            <p className="mt-2">Bienvenido de nuevo {session.user?.name}</p>
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+          </div>
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
         </div>
-    )
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
