e3c936d43bb68c79a9c4fd9d6c7b237b95cd2fc9
yerayboix <yerayboix@gmail.com>
2025-10-24 18:20:08 +0200

    feat(layouts): added custom layouts to projects and dashboard routes.

---
+++ web/app/(projects)/projects/breadcrum.tsx
@@ -0,0 +1,24 @@
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-separator";
+
+export default function ProjectBreadcrumb() {
+    return (
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>Proyectos</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </div>
+        </header>
+    )
+}
---
+++ web/app/(projects)/projects/page.tsx
@@ -0,0 +1,12 @@
+import ProjectBreadcrumb from "./breadcrum";
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <ProjectBreadcrumb />
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        
+      </div>
+    </>
+  )
+}
